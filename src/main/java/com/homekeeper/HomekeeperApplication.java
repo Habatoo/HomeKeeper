23da589ac6823bb42726c16ab00b72b7b5ab18ad
Habatoo <habatoo@gmail.com>
2020-12-20 16:08:16 +0700

    initial commit

---
+++ src/main/java/com/homekeeper/HomekeeperApplication.java
@@ -0,0 +1,13 @@
+package com.homekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomekeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomekeeperApplication.class, args);
+	}
+
+}
